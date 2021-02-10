329e9a0acde6e2aa19b1e581f64d2f7c6dc28ff4
Sascha Adler <sadler@crkn.ca>
2021-02-10 14:05:45 -0330

    Adds a few more things into the main export

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-const Kivik = require("./Kivik");
+const Container = require("./Container");
 const Instance = require("./Instance");
+const Kivik = require("./Kivik");
+const Validator = require("./Validator");
 
 module.exports = {
   default: Instance,
   Kivik,
   Instance,
+  Container,
+  Validator,
 };
