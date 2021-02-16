bd443edd6e38d2228c543bd5f1b299369c5cb7d2
Sascha Adler <sadler@crkn.ca>
2021-02-16 15:26:14 -0330

    Schema sourcing refactor

--- src/getValidator.js
+++ src/getValidator.js
@@ -6,6 +6,16 @@ const addFormats = require("ajv-formats").default;
 const globby = require("globby");
 const { withDefaults } = require("./options");
 
+const addSchemasFrom = async (ajv, options) => {
+  const { include, exclude, cwd, keyFunc } = options;
+  const paths = await globby(include, { exclude, cwd, absolute: true });
+  await Promise.all(
+    paths.map(async (fullPath) => {
+      ajv.addSchema(await fs.readJSON(fullPath), keyFunc(fullPath));
+    })
+  );
+};
+
 const fromURL = async (input) => {
   try {
     const url = new URL(input);
@@ -33,38 +43,21 @@ module.exports = async (directory, options = {}) => {
   // TODO: config for which formats to add?
   addFormats(ajv);
 
-  const schemas = [];
   const cwd = path.resolve(directory);
-
-  schemas.push(
-    ...(
-      await globby("schemas/*.json", {
-        cwd: cwd,
-        absolute: true,
-      })
-    ).map((fullPath) => {
-      return [fullPath, path.basename(fullPath, ".json")];
-    })
-  );
-
   const schemaMap = (glob) => path.posix.join(glob, "schema.json");
-  schemas.push(
-    ...(
-      await globby(include.map(schemaMap), {
-        ignore: exclude.map(schemaMap),
-        cwd: cwd,
-        absolute: true,
-      })
-    ).map((fullPath) => {
-      return [fullPath, path.basename(path.dirname(fullPath))];
-    })
-  );
 
-  await Promise.all(
-    schemas.map(async ([path, key]) => {
-      ajv.addSchema(await fs.readJSON(path), key);
-    })
-  );
+  await addSchemasFrom(ajv, {
+    include: "schemas/*.json",
+    exclude: [],
+    cwd,
+    keyFunc: (fullPath) => path.basename(fullPath, ".json"),
+  });
+  await addSchemasFrom(ajv, {
+    include: include.map(schemaMap),
+    exclude: exclude.map(schemaMap),
+    cwd,
+    keyFunc: (fullPath) => path.basename(path.dirname(fullPath)),
+  });
 
   return (key) => {
     const validate = ajv.getSchema(key);
